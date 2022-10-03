e8fa4e1a358d4502695e8b0f71cfd58ef3ac3fa4
ismaelsousa <ismaelsousa1998@outlook.com>
2022-10-03 10:18:25 -0300

    first commit

---
+++ MyApp/App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+import {useState} from 'react';
+import {SafeAreaView, StatusBar, Text, Button} from 'react-native';
+import RTNCalculator from 'rtn-calculator/js/NativeCalculator';
+
+const App = () => {
+  const [result, setResult] = useState<number | null>(null);
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'dark-content'} />
+      <Text style={{marginLeft: 20, marginTop: 20}}>3+7={result ?? '??'}</Text>
+      <Button
+        title="Compute"
+        onPress={async () => {
+          const value = await RTNCalculator?.add(3, 7);
+          if (value) {
+            setResult(value);
+          }
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+export default App;
